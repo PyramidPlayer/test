8591c4f936e8e8946791cc00f6d14f88b81d02e8
PyramidPlayer <toroptsev@gmail.com>
2022-05-12 04:59:51 +0300

    db3

--- db3.sql
+++ db3.sql
@@ -1,35 +1,3 @@
-create table teachers (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	name TEXT NOT NULL,
-	surname TEXT NOT NULL,
-	email TEXT NOT NULL
-);
-
-create table courses (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	name TEXT NOT NULL
-);
-
-create table streams (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	course_id INTEGER NOT NULL,
-	number TEXT NOT NULL,
-	starts_at TEXT NOT NULL,
-	students_count INTEGER NOT NULL,
-	FOREIGN KEY (course_id) REFERENCES courses(id)
-);
-
-create table grades (
-	teacher_id INTEGER NOT NULL,
-	stream_id INTEGER NOT NULL,
-	grade REAL,
-	PRIMARY KEY (teacher_id, stream_id),
-	FOREIGN KEY (teacher_id) REFERENCES teachers(id),
-	FOREIGN KEY (stream_id) REFERENCES streams(id)
-);
-
-#------------------------------------------------------
-
 alter table streams rename column starts_at to started_at;
 alter table streams add column finished_at TEXT;
 
@@ -54,5 +22,3 @@ insert into grades (teacher_id, stream_id, grade) values (3, 1, 4.7);
 insert into grades (teacher_id, stream_id, grade) values (2, 2, 4.9);
 insert into grades (teacher_id, stream_id, grade) values (1, 3, 4.8);
 insert into grades (teacher_id, stream_id, grade) values (1, 4, 4.9);
-
-# alter table grades change column stream_id REAL;
